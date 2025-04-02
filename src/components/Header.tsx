f50b49f1582f92ce9269edb123f44f190bf1f9e6
German <BinderGerman>
2025-04-01 21:39:06 -0300

    added hyperlink to the slider image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -79,7 +79,8 @@ export default function Header() {
   );
 }*/
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState} from "react";
+import { Link } from "react-router-dom"
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
@@ -127,81 +128,83 @@ export default function Header() {
 
         {featuredPosts.map((post) => (
           <SwiperSlide key={post.slug.current}>
-            <div
-              style={{
-                position: "relative",
-                height: "400px", // Ajusta el tamaño si es necesario
-                //backgroundColor: "#000",
-              }}
-            >
-              {/* Fondo parallax de la imagen del post */}
-              {post.mainImage && (
+            <a href={`/post/${post.slug.current}`} style={{ textDecoration: 'none' }}>
                 <div
-                  style={{
+                style={{
+                    position: "relative",
+                    height: "400px", // Ajusta el tamaño si es necesario
+                    //backgroundColor: "#000",
+                }}
+                >
+                {/* Fondo parallax de la imagen del post */}
+                {post.mainImage && (
+                    <div
+                    style={{
+                        position: "absolute",
+                        top: "0",
+                        left: "0",
+                        right: "0",
+                        bottom: "0",
+                        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${urlFor(post.mainImage).url()})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        //filter: "brightness(0.1)", // Para darle un filtro oscuro y hacer el texto legible
+                        zIndex: -1,
+                    }}
+                    data-swiper-parallax="-30%"
+                    />
+                )}
+
+                {/* Título y texto del post */}
+                <div
+                    className="title"
+                    style={{
                     position: "absolute",
-                    top: "0",
-                    left: "0",
-                    right: "0",
-                    bottom: "0",
-                    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${urlFor(post.mainImage).url()})`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                    //filter: "brightness(0.1)", // Para darle un filtro oscuro y hacer el texto legible
-                    zIndex: -1,
-                  }}
-                  data-swiper-parallax="-30%"
-                />
-              )}
+                    top: "65%",
+                    left: "2%",
+                    transform: "translateX(-50%)",
+                    color: "white",
+                    fontSize: "2rem",
+                    fontWeight: "bold",
+                    }}
+                    data-swiper-parallax="-300"
+                >
+                    {post.title}
+                </div>
 
-              {/* Título y texto del post */}
-              <div
-                className="title"
-                style={{
-                  position: "absolute",
-                  top: "65%",
-                  left: "2%",
-                  transform: "translateX(-50%)",
-                  color: "white",
-                  fontSize: "2rem",
-                  fontWeight: "bold",
-                }}
-                data-swiper-parallax="-300"
-              >
-                {post.title}
-              </div>
+                <div
+                    className="excerpt"
+                    style={{
+                    position: "absolute",
+                    top: "75%",
+                    left: "2%",
+                    transform: "translateX(-50%)",
+                    color: "white",
+                    fontSize: "1.5rem",
+                    fontWeight: "lighter",
+                    }}
+                    data-swiper-parallax="-200"
+                >
+                    {post.excerpt}
+                </div>
 
-              <div
-                className="excerpt"
-                style={{
-                  position: "absolute",
-                  top: "75%",
-                  left: "2%",
-                  transform: "translateX(-50%)",
-                  color: "white",
-                  fontSize: "1.5rem",
-                  fontWeight: "lighter",
-                }}
-                data-swiper-parallax="-200"
-              >
-                {post.excerpt}
-              </div>
-
-              {/* Categoría 
-              <div
-                className="category"
-                style={{
-                  position: "absolute",
-                  top: "60%",
-                  left: "50%",
-                  transform: "translateX(-50%)",
-                  color: "white",
-                  fontSize: "1.2rem",
-                }}
-                data-swiper-parallax="-100"
-              >
-                {post.category ? post.category.title : "Sin categoría"}
-              </div>*/}
-            </div>
+                {/* Categoría 
+                <div
+                    className="category"
+                    style={{
+                    position: "absolute",
+                    top: "60%",
+                    left: "50%",
+                    transform: "translateX(-50%)",
+                    color: "white",
+                    fontSize: "1.2rem",
+                    }}
+                    data-swiper-parallax="-100"
+                >
+                    {post.category ? post.category.title : "Sin categoría"}
+                </div>*/}
+                </div>
+            </a>
           </SwiperSlide>
         ))}
       </Swiper>
