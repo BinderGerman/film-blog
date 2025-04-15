900750fc5aaaae70ffd23512aa2c65b6cdce7a1c
German <BinderGerman>
2025-04-15 18:18:40 -0300

    Refactoring CategoryColumn.astro

--- src/types/category.ts
+++ src/types/category.ts
@@ -1,6 +1,9 @@
+import type { ImageAsset } from "@sanity/types";
+
 export interface Category {
   _id: string;
   title: string;
   slug: { current: string };
+  mainImage?: ImageAsset & { alt?: string };
   description?: string;
 }
--- src/utils/database.ts
+++ src/utils/database.ts
@@ -46,6 +46,7 @@ export async function getCategories(): Promise<Category[]> {
         _id,
         title,
         slug,
+        mainImage,
         description
       }`
   );
