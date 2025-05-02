79531a4b8b02b3a12101e2f5c9ea2937f1694e73
BinderGerman <bindergerman@gmail.com>
2025-05-01 22:27:44 -0300

    Added tailwind.config-mjs and Font to FontSource

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}", // ajustá si usás otros formatos
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        serif: ['PT Serif', ...defaultTheme.fontFamily.serif],
+      },
+    },
+  },
+  plugins: [],
+};
