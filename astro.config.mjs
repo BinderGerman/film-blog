5089f37679e6d78de21eeb3cfda0cef851d28b40
BinderGerman <bindergerman@gmail.com>
2025-05-11 19:02:27 -0300

    Added sitemap integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,20 +6,20 @@ import react from '@astrojs/react';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [
-    sanity({
-      projectId: 'bn4rvp7h',
-      dataset: 'production',
-      useCdn: false, // Si es estático va false. Si es dinámico va true 
-      apiVersion: "2025-03-29", 
-      studioBasePath: '/studio' // De esta manera integramos Sanity Studio a una ruta de Astro
-    }),
-    react()
-  ],
+  site: 'https://losolvidosdecronos.com.ar/',
+  integrations: [sanity({
+    projectId: 'bn4rvp7h',
+    dataset: 'production',
+    useCdn: false, // Si es estático va false. Si es dinámico va true 
+    apiVersion: "2025-03-29", 
+    studioBasePath: '/studio' // De esta manera integramos Sanity Studio a una ruta de Astro
+  }), react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
     resolve: {
